da7821d4d5236feed9d2caa78e7fb95c130ed464
Christoph Sayer <christophsayer@gmail.com>
2017-02-21 11:56:50 +0200

    Working

--- src/BatteryMediator.java
+++ src/BatteryMediator.java
@@ -2,6 +2,33 @@
  * Created by bernd on 21.02.2017.
  */
 
-public class BatteryMediator extends Subscriber {
+import com.google.common.eventbus.*;
 
+public class BatteryMediator extends Subscriber {
+    private String identifier;
+
+    public BatteryMediator(String identifier) {
+        // TODO Portverbindung zu den Komponenten
+        this.identifier = identifier;
+    }
+
+    @Subscribe
+    public void receive(OnEvent event) {
+
+    }
+
+    @Subscribe
+    public void receive(OffEvent event) {
+
+    }
+
+    @Subscribe
+    public void receive(ChargeEvent event) {
+
+    }
+
+    @Subscribe
+    public void receive(EjectEvent event) {
+
+    }
 }
