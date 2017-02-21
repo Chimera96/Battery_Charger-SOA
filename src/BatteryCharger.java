cd2bf7d852d6a435b1d69faa2810d7f0a63c4dd4
Chimera96 <bernd.hartmann96@gmx.de>
2017-02-21 10:53:40 +0100

    guava

---
+++ src/BatteryCharger.java
@@ -0,0 +1,10 @@
+import com.google.common.eventbus.EventBus;
+/**
+ * Created by bernd on 21.02.2017.
+ */
+public class BatteryCharger {
+
+    private String name, type;
+    private EventBus eventBus;
+
+}
